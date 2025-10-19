8f99492ee1397e1eca72900f4c28b3e074095880
JDMQXD <juanda@mx>
2025-10-19 17:50:52 -0500

    se termina

--- src/plugins/sound_plugin.rs
+++ src/plugins/sound_plugin.rs
@@ -13,11 +13,11 @@ pub struct SoundPlugin;
 
 impl Plugin for SoundPlugin {
     fn build(&self, app: &mut App) {
-        // No hace falta volver a agregar AudioPlugin porque ya está en DefaultPlugins
+       
         app
-            // Registrar el tipo de asset de audio (necesario para cargar archivos .ogg)
+            
             .init_asset::<AudioSource>()
-            // Registrar eventos y sistemas
+            
             .add_event::<GoatAteGrassEvent>()
             .add_systems(Startup, load_eat_sound)
             .add_systems(Update, play_eat_sound);
@@ -25,7 +25,6 @@ impl Plugin for SoundPlugin {
 }
 
 fn load_eat_sound(asset_server: Res<AssetServer>, mut commands: Commands) {
-    // La ruta debe ser relativa a la carpeta "assets"
     let handle = asset_server.load("NOM.ogg");
     commands.insert_resource(EatSound { handle });
 }
